bf2c046cd8aed6854b603c50348b2286800feafc
Eldar Rizaev <eldarrizaevsw@mail.ru>
2023-02-23 15:21:52 +0100

    add

---
+++ recipes/urls.py
@@ -0,0 +1,33 @@
+"""recipes URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from urllib import request
+
+from django.contrib import admin
+from django.urls import path, include
+
+from recipe_basis.views import *
+
+from recipe_basis.views import menuMain, omlet
+
+urlpatterns = [
+    # здесь зарегистрируйте вашу view-функцию
+
+
+	path("", menuMain, name="index"),
+	path("omlet/", omlet, name="omlet"),
+	path("calc/", include("calculator.urls")),
+
+]
